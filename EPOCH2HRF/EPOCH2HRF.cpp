6f3a0b08229b38d03a4b4a057efc9c4c89060790
Ossi Heino <ossi.heino@gmail.com>
2021-01-22 14:01:22 +0200

    Ready

---
+++ EPOCH2HRF/EPOCH2HRF.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include "csv.h"
+
+std::string convert(long int seconds);
+
+
+int main()
+{
+	std::fstream fin;
+	std::vector<std::vector<std::string>> row;
+	std::string line, word, temp;
+
+	// csv file
+	//fin.open("D:/csv_epoch.csv", std::ios::in);
+
+    io::CSVReader<2> in("D:/csv_epoch.csv");
+    std::string unixEpoch;
+    std::string ipAddress;
+
+    while (in.read_row(unixEpoch, ipAddress)) {
+        std::string temp = convert(std::stol(unixEpoch));
+
+        std::cout << "epoch: " << temp << std::endl;
+
+        std::cout << "ip: " << ipAddress << std::endl;
+
+    }
+
+}
+
+
+using namespace std;
+
+string convert(long int seconds) {
+
+    // Save the time in Human
+    // readable format
+    string ans = "";
+
+    // Number of days in month
+    // in normal year
+    int daysOfMonth[] = { 31, 28, 31, 30, 31, 30,
+                          31, 31, 30, 31, 30, 31 };
+
+    long int currYear, daysTillNow, extraTime,
+        extraDays, index, date, month, hours,
+        minutes, secondss, flag = 0;
+
+    // Calculate total days unix time T
+    daysTillNow = seconds / (24 * 60 * 60);
+    extraTime = seconds % (24 * 60 * 60);
+    currYear = 1970;
+
+    // Calculating currrent year
+    while (daysTillNow >= 365) {
+        if (currYear % 400 == 0
+            || (currYear % 4 == 0
+                && currYear % 100 != 0)) {
+            daysTillNow -= 366;
+        }
+        else {
+            daysTillNow -= 365;
+        }
+        currYear += 1;
+    }
+
+    // Updating extradays because it
+    // will give days till previous day
+    // and we have include current day
+    extraDays = daysTillNow + 1;
+
+    if (currYear % 400 == 0
+        || (currYear % 4 == 0
+            && currYear % 100 != 0))
+        flag = 1;
+
+    // Calculating MONTH and DATE
+    month = 0, index = 0;
+    if (flag == 1) {
+        while (true) {
+
+            if (index == 1) {
+                if (extraDays - 29 < 0)
+                    break;
+                month += 1;
+                extraDays -= 29;
+            }
+            else {
+                if (extraDays
+                    - daysOfMonth[index]
+                    < 0) {
+                    break;
+                }
+                month += 1;
+                extraDays -= daysOfMonth[index];
+            }
+            index += 1;
+        }
+    }
+    else {
+        while (true) {
+
+            if (extraDays
+                - daysOfMonth[index]
+                < 0) {
+                break;
+            }
+            month += 1;
+            extraDays -= daysOfMonth[index];
+            index += 1;
+        }
+    }
+
+    // Current Month
+    if (extraDays > 0) {
+        month += 1;
+        date = extraDays;
+    }
+    else {
+        if (month == 2 && flag == 1)
+            date = 29;
+        else {
+            date = daysOfMonth[month - 1];
+        }
+    }
+
+    // Calculating HH:MM:YYYY
+    hours = extraTime / 3600;
+    minutes = (extraTime % 3600) / 60;
+    secondss = (extraTime % 3600) % 60;
+
+    ans += to_string(date);
+    ans += "/";
+    ans += to_string(month);
+    ans += "/";
+    ans += to_string(currYear);
+    ans += " ";
+    ans += to_string(hours);
+    ans += ":";
+    ans += to_string(minutes);
+    ans += ":";
+    ans += to_string(secondss);
+
+    // Return the time
+    return ans;
+}
+
